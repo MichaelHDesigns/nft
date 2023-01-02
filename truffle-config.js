b5d196558fda9d22defc6a2141f128e05e4c344d
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-02 00:55:17 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -3,8 +3,8 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 7545,
-      network_id: '*',
+      port: 8545,
+      network_id: '2330',
     },
   },
   compilers: {
