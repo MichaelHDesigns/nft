9a856a14b486e2fb8308f0d43a034578d4bbbaff
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-02 03:26:15 -0500

    Update index.tsx

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -20,7 +20,7 @@ const NftItem: FunctionComponent<NftItemProps> = ({ item, buyNft }) => {
       </div>
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
-          <p className="text-sm font-medium text-indigo-600">Creatures NFT</p>
+          <p className="text-sm font-medium text-indigo-600">ALT NFT</p>
           <div className="block mt-2">
             <p className="text-xl font-semibold text-gray-900">{meta.name}</p>
             <p className="mt-3 mb-3 text-base text-gray-500">
